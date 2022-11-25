7b1479ace1eb68944a91a32aceb46593d0e7eccd
AnimeshKotka <animeshk@capitalnumbers.com>
2022-11-25 17:10:35 +0530

    production ready code is done

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header.component";
-import LotteryEntrance from "../components/LotteryEntrance";
+import LotteryEntrance from "../components/LotteryEnterence";
 import Web3Header from "../components/Web3Header";
+import { useMoralis } from "react-moralis";
+const supportedChains = ["31337", "5"];
 
 export default function Home() {
+  const { isWeb3Enabled, chainId } = useMoralis();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,10 +20,22 @@ export default function Home() {
 
       {/* <Header /> */}
       <Web3Header />
-      <LotteryEntrance />
+      {isWeb3Enabled ? (
+        <div>
+          {supportedChains.includes(parseInt(chainId).toString()) ? (
+            <div className="flex flex-row">
+              <LotteryEntrance className="p-8" />
+            </div>
+          ) : (
+            <div>{`Please switch to a supported chainId. The supported Chain Ids are: ${supportedChains}`}</div>
+          )}
+        </div>
+      ) : (
+        <div>Please connect to a Wallet</div>
+      )}
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/AnimeshKotka"
           target="_blank"
           rel="noopener noreferrer"
         >
