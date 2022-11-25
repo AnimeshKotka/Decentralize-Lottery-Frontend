391f1419b552771aadb96d617e352f6ae526c808
AnimeshKotka <animeshk@capitalnumbers.com>
2022-11-25 18:40:07 +0530

    out directory added

---
+++ out/_next/static/ovrFClE8EWY4pmk74E1XO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-8585a3b3ff7a6113.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
